85204757fe17a0109e2760417c9530e1d5eb0204
semapq <aksurtlive@gmail.com>
2023-03-27 02:51:18 +0300

    main file update
    Added some explanations and update comments in code

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
-//include block
+/* Include block */
 #include <Arduino.h>
 #include <Servo.h>
 #include <IBusBM.h>
 
 
-//define block
+/* Define block */
 #define MAX_SIGNAL 2000
 #define MIN_SIGNAL 1000
-#define MOTORA_PIN 24
-#define MOTORB_PIN 25
 
-//variables block
+/*
+ *  The moment of explanation
+ *  This project created using PlatformIO, therefore, the configuration of pins goes
+ *  a little differently than in ArduinoIDE, in my case it was necessary to specify exactly
+ *  the pin number, designating the contact number in the datasheet.
+ *  Then, if you using ArduinoIDE, change pin defines!
+ */
+
+#define MOTORA_PIN 24 /* Change to PB5 if using ArduinoIDE, 24 is chip pin nubmer */
+#define MOTORB_PIN 25 /* Change to PB6 if using ArduinoIDE, 25 is chip pin number */
+
+/* Variables block */
 int speed = MIN_SIGNAL;
 int speedA = MIN_SIGNAL;
 int speedB = MIN_SIGNAL;
@@ -25,11 +34,11 @@ float k_mxspd = 1;
 bool stop = 0;
 bool calibration_boot = 0;
 
-//init block
+/* Init block */
 IBusBM ibus;
 Servo motorA, motorB;
 
-//func block
+/* Func block */
 int readChannel(byte channelInput, int minLimit, int maxLimit, int defaultValue);
 bool readSwitch(byte channelInput, bool defaultValue);
 void ESC_calibration();
@@ -55,6 +64,8 @@ void setup() {
 void loop() {
 
   digitalWrite(13, HIGH);
+
+  /* Channel Debug */
   // Serial.print("ch1: "); Serial.print(ibus.readChannel(1));
   // Serial.print("\tch3: "); Serial.print(map(ibus.readChannel(3), 1000, 2000, -1000, 1000));
   // Serial.print("\tch9: "); Serial.println(ibus.readChannel(9));
@@ -84,15 +95,16 @@ void loop() {
   if(speedB > max_speed) speedB = max_speed;
   if(speedB < 1000) speedB = 1000;
 
-  Serial.print("spd: "); Serial.print(speed);
-  Serial.print("\ttrn: "); Serial.print(turn);
-  Serial.print("\tmxspd: "); Serial.print(max_speed);
-  Serial.print("\tk_mxspd: "); Serial.print(k_mxspd);
-
-  Serial.print("\tspdA: "); Serial.print(speedA);
-  Serial.print("\tspdB: "); Serial.print(speedB);
-  
-  Serial.print("\tstp: "); Serial.println(stop);
+/* Variables Debug */
+//  Serial.print("spd: "); Serial.print(speed);
+//  Serial.print("\ttrn: "); Serial.print(turn);
+//  Serial.print("\tmxspd: "); Serial.print(max_speed);
+//  Serial.print("\tk_mxspd: "); Serial.print(k_mxspd);
+//
+//  Serial.print("\tspdA: "); Serial.print(speedA);
+//  Serial.print("\tspdB: "); Serial.print(speedB);
+//
+//  Serial.print("\tstp: "); Serial.println(stop);
 
   if(stop)
   {
